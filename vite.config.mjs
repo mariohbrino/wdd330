f33b45f6e7be3fb8554541b0f74365c64473ea88
Mario Brino <mariobrino@gmail.com>
2026-07-30 21:41:41 -0600

    feat: add cards input to Vite configuration for build process

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         siteplan: resolve(__dirname, "src/siteplan.html"),
+        cards: resolve(__dirname, "src/cards.html"),
       },
     },
   },
